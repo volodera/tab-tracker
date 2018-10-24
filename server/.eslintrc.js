31cacc3c646bbaf040965afa80d79ab5078aa6b7
Alen Voloder <volodera@gmail.com>
2018-10-24 20:31:23 +0200

    lala

---
+++ server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "root": true,
+    "env": {
+     "es6": true,
+     "node": true,
+     "browser": true
+    },
+    "extends": [ "airbnb", "plugin:import/warnings" ],
+    "parserOptions": {
+     "ecmaVersion": 6,
+     "sourceType": "module",
+     "ecmaFeatures": {
+      "jsx": true
+     }
+    },
+    "rules": {
+        // windows linebreaks when not in production environment
+        "linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"]
+    }
+   };
